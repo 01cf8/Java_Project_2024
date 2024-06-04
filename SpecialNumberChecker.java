562e2c855b9d70b6ad857e4f75c396d34c51a67c
01cf8 <akshitdob14@gmail.com>
2024-06-04 21:00:46 +0530

    Java_Project_2024

--- SpecialNumberChecker.java
+++ SpecialNumberChecker.java
@@ -1,41 +1,32 @@
 import java.util.Scanner;
 
 public class SpecialNumberChecker {
-    // Method to calculate factorial of a number
-    public static int factorial(int n) {
-        int result = 1;
-        for (int i = 1; i <= n; i++) {
-            result *= i;
-        }
-        return result;
-    }
-
-    // Method to check if a number is a Special number
-    public static boolean isSpecial(int number) {
-        int originalNumber = number;
-        int sum = 0;
-
-        while (number != 0) {
-            int digit = number % 10;
-            sum += factorial(digit);
-            number /= 10;
-        }
-
-        return sum == originalNumber;
-    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Prompt user for the input number
         System.out.print("Enter a number: ");
         int number = sc.nextInt();
 
-        // Check if the number is a Special number
-        if (isSpecial(number)) {
-            System.out.println(number + " is a Special number.");
+        int originalNumber = number;
+        int sum = 0;
+
+        // Calculate sum of factorials
+        while (number > 0) {
+            int digit = number % 10;
+            int factorial = 1;
+            for (int i = 1; i <= digit; i++) {
+                factorial *= i;
+            }
+            sum += factorial;
+            number /= 10;
+        }
+
+        // Check if the sum of factorials is equal to the original number
+        if (sum == originalNumber) {
+            System.out.println(originalNumber + " is a special number.");
         } else {
-            System.out.println(number + " is not a Special number.");
+            System.out.println(originalNumber + " is not a special number.");
         }
 
         sc.close();
